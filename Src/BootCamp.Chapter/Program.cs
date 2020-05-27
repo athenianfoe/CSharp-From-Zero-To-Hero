0b4c89dfd2cbb79cfa56a2e58e5d46bffdb55dc0
irfan0113 <irfanera1331@gmail.com>
2020-05-27 13:20:55 +0530

    Bmi calculation added

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -2,11 +2,31 @@
 
 namespace BootCamp.Chapter
 {
-    class Program
+    class Programnnnnnn
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var name = Enter_input();
+            var surname = Enter_input();
+            var age = Enter_input();
+            var weight = Enter_input();
+            var height = Enter_input();
+            Console.WriteLine(name + " " + surname + " is " + age + " years old, his weight is " + weight + " kg and his height is " + height);
+
+            var sum = calc_bmi(double.Parse(height), double.Parse(weight));
+            Console.WriteLine(name + "\'s" + " BMI is " + sum);
+         
+
+        }
+
+        private static string Enter_input()
+        {
+            return Console.ReadLine();
+        }
+
+        private static Double calc_bmi(double height, double weight)
+        {
+            return weight / Math.Pow((height*0.01), 2 );
         }
     }
 }
